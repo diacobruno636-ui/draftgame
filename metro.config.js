a451d07887619534f3f036562f31ab3d4adfbf6e
diacobruno636-ui <diacobruno636@gmail.com>
2025-12-04 14:59:20 -0300

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,15 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withRorkMetro } = require('@rork-ai/toolkit-sdk/metro');
+const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withRorkMetro(config);
-
+// Only apply Rork Metro on native builds
+if (process.env.EXPO_PLATFORM !== "web") {
+  try {
+    const { withRorkMetro } = require("@rork-ai/toolkit-sdk/metro");
+    module.exports = withRorkMetro(config);
+  } catch {
+    module.exports = config;
+  }
+} else {
+  module.exports = config;
+}
